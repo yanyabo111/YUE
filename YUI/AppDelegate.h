eccabc27e37b518e4fc74ffd9af3ff7a2850260c
晏亚博 <yanyabo111@gmail.com>
2015-10-12 19:48:38 +0800

    init

---
+++ YUI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YUI
+//
+//  Created by 晏亚博 on 15/10/12.
+//  Copyright © 2015年 晏亚博. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YUI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YUI
+//
+//  Created by 晏亚博 on 15/10/12.
+//  Copyright © 2015年 晏亚博. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
